98cbab678ffcbfc9a0f275ddcd539d724b2e74ed
Henrique Nasciment0 <hsncorretor@gmail.com>
2024-11-05 07:13:15 -0300

    feat: user updade, findById and delete

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
@@ -10,8 +19,26 @@ export class UserController {
     return this.userService.getUsers();
   }
 
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById({ id: Number(id) });
+  }
+
   @Post()
   createUser(@Body() data: { name: string; email: string; password: string }) {
     return this.userService.createUser(data);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() data: Prisma.UserUpdateInput) {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data,
+    });
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser({ id: Number(id) });
   }
 }
